00fb62e138f3be89db2bbcfff2f7837013719dbe
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-03-09 23:03:41 -0800

    improved images

--- app/routes/pacific/fragments/stripe-washington.tsx
+++ app/routes/pacific/fragments/stripe-washington.tsx
@@ -1,7 +1,9 @@
 export default function Washington() {
  return (
-  <div id='Washington' className='flip-in-hor-top'>
-   <img src='/stripes_wa.png'/>
+  <div id='Washington' className='flip-in-hor-top hero'>
+   <div className='img'>
+    <img src='/stripes_wa.png' alt='"Warming stripes" visualization for the state of Washington, showing an overall increase in warm years and the near-vanishment of cold ones.'></img>
+   </div>
   </div>
  )
 }
