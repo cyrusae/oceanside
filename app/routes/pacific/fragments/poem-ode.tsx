fcd7497baac86418a100c732e07910423c04fd80
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-03-10 00:48:02 -0800

    Add poems and update CSS

---
+++ app/routes/pacific/fragments/poem-ode.tsx
@@ -0,0 +1,15 @@
+export default function Ode() {
+ return (
+  <div id='Ode' className='poem'>
+   <img src='' />
+   <div className='quote'>
+    forgetting the nation is a syndrome.<br/>
+    All that’s left of the sea in you is foam,<br/>
+    the coastline's broken voice and all its crags.
+   </div>
+   <div className='credit'>
+    Urayoán Noel,<br/>"No Longer Ode"
+   </div>
+  </div>
+ )
+}
---
+++ app/routes/pacific/fragments/poem-winsum.tsx
@@ -0,0 +1,14 @@
+export default function WinSum() {
+ return (
+  <div id='WinSum' className='poem'>
+   <img src='' />
+   <div className='quote'>
+    &nbsp;&nbsp;sit in the center of the poem, invigorated<br/>
+    with inky awkward blankness.<br/>
+    The bottom teeth of summer<br/>
+    in winter chattering: here’s the moon. 
+   </div>
+   <div className='credit'>Noah Eli Gordon,<br/>"Summer in Winter in Summer"</div>
+  </div>
+ )
+}
