c81523e409585da2fd42e6fa038b373ba161c958
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-03-10 19:09:05 -0800

    Added Kyle Whyte quote

---
+++ app/routes/pacific/fragments/quote-clime.tsx
@@ -0,0 +1,13 @@
+export default function Define() {
+ return (
+  <div id='define' className='cite'>
+   <img src='/purple_rhombus_quote_bg_ph.png' className='fade-in PLACEHOLDER'/>
+   <div className='quote text-focus-in'>
+    <p>Consider climate change as about <b>the timing of relationships</b> we have with all that's around us.</p>
+   </div>
+   <div className='credit text-focus-in'>
+    Kyle Whyte,<br/>"Braiding Kinship and Time"
+   </div>
+  </div>
+ )
+}
