82d346029c257f7ab4f5bc0b4643d53c3e04838f
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-03-12 15:42:53 -0800

    Whamline refinements pt. 1
    Shifted last image outside div with backgrounds to make it its own thing

--- app/routes/pacific/fragments/stripe-whamline.tsx
+++ app/routes/pacific/fragments/stripe-whamline.tsx
@@ -1,7 +1,8 @@
 
 export default function Wham() {
  return (
-  <div id='kicker'><img src='/climate_stripes_mx.png' aria-describedby="" aria-details="" className='fade-in' />
+  <div id='kicker'>
+   <div id='kimg'><img src='/climate_stripes_mx.png' aria-describedby="" aria-details="" className='fade-in' /></div>
    </div>
  )
 }
