1e46e638ac1a7ec05e1ff83508ede4fc6df2e45b
cyrusae <86699936+cyrusae@users.noreply.github.com>
2022-03-16 15:35:07 -0700

    Alt text/caption refinements
    WIP

--- app/routes/pacific/fragments/stripe-ohio.tsx
+++ app/routes/pacific/fragments/stripe-ohio.tsx
@@ -1,7 +1,7 @@
 export default function Ohio() {
  return (
-  <div id='ohio' className='hero'>
-   <img src='/stripes_oh.png' />
+  <div id='ohio' className='hero' role='img'>
+   <img src='/stripes_oh.png' alt=''/>
   </div>
  )
 }
